bfcb6a6d7f2e0210e6a29a1188a4ba4e7022d447
Isaiah Davis <ID@Isaiahs-iMac.local>
2021-02-08 18:13:39 -0500

    commit tree d097c299ab58a11d441931a3b99802ae101ac986

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+  if speed > 60 
+    true  
+  elsif speed < 40
+    true  
+  else speed == 50 
+    false 
+  end  
 end
-
 
 
 def not_safe?(speed)
-	
+    speed > 60 || 40 > speed ? true : false   
 end
-	
-
-
